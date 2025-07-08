91f626d32139d61f5a2ef9ce6aef7cd6c393de36
Carlos magno <83795938+Magno-programmer@users.noreply.github.com>
2025-07-07 21:43:02 -0300

    adicionando header ao inicio

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,16 +2,20 @@
 import React from 'react';
 
 // Importing desktop components
+import Header from '@/components/desktopVersion/commonPages/header/Header';
 import HeroSection from '@/components/desktopVersion/Index/HeroSection';
 import TestimonialSection from '@/components/desktopVersion/Index/TestimonialSection';
 import Footer from '@/components/desktopVersion/commonPages/footer/Footer';
 import WhatsAppButton from '@/components/globalComponents/WhatsAppButton';
 
 // Importing mobile components
+import HeaderMobile from '@/components/mobileVersion/commonPages/header/HeaderMobile';
 import HeroSectionMobile from '@/components/mobileVersion/Index/HeroSectionMobile';
 import FooterMobile from '@/components/mobileVersion/commonPages/footer/FooterMobile';
 import TestimonialSectionMobile from '@/components/mobileVersion/Index/TestimonialSectionMobile';
 
+// Importing tablet components
+import HeaderTablet from '@/components/tabletVersion/commonPages/header/HeaderTablet';
 import HeroSectionTablet from '@/components/tabletVersion/Index/HeroSectionTablet';
 import FooterTablet from '@/components/tabletVersion/commonPages/footer/FooterTablet';
 import TestimonialSectionTablet from '@/components/tabletVersion/Index/TestimonialSectionTablet';
@@ -21,6 +25,7 @@ const Index = () => {
     <>
       {/* Mobile Version */}
       <div className="block sm:hidden">
+        <HeaderMobile />
         <HeroSectionMobile />
         <TestimonialSectionMobile />
         <FooterMobile />
@@ -29,6 +34,7 @@ const Index = () => {
 
       {/* Tablet Version */}
       <div className="hidden sm:block lg:hidden">
+        <HeaderTablet />
         <HeroSectionTablet />
         <TestimonialSectionTablet />
         <FooterTablet />
@@ -37,6 +43,7 @@ const Index = () => {
 
       {/* Desktop Version */}
       <div className="hidden lg:block">
+        <Header />
         <HeroSection />
         <TestimonialSection />
         <Footer />
